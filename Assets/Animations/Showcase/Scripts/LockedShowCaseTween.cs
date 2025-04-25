62ab36be28470b63918b7cc8c4539a2e8063c5e7
yuvalarie <yuvalarie@gmail.com>
2025-04-25 17:45:10 +0300

    Added showcase animation (doors+lock)

---
+++ Assets/Animations/Showcase/Scripts/LockedShowCaseTween.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using DG.Tweening;
+using UnityEngine;
+
+namespace Animations.Showcase.Scripts
+{
+    public class LockedShowCaseTween : MonoBehaviour
+    {
+        [SerializeField] private float duration = 0.3f;
+        [SerializeField] private float strength = 0.2f;
+        [SerializeField] private int vibrato = 10;
+
+        public void PlayLockedAnimation()
+        {
+            transform.DOShakePosition(duration, strength, vibrato, 90, false, true);
+        }
+    }
+}
