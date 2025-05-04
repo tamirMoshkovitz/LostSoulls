73c794b73696b8a5087347002c0472b3400ec22d
eladfirer <elad.firer@mail.huji.ac.il>
2025-05-04 23:24:11 +0300

    dixed delay before going to top down problem.

--- Assets/Game Flow/CollectableObjects/CollectableManDollObject.cs
+++ Assets/Game Flow/CollectableObjects/CollectableManDollObject.cs
@@ -16,29 +16,10 @@ namespace Game_Flow.CollectableObjects
         /// </summary>
         public void OnCollect(Action callback)
         {
-            StartCoroutine(MakeAnimationOfDoll(callback));
-        }
-
-        private IEnumerator MakeAnimationOfDoll(Action callback)
-        {
-            // 1) Freeze time (and thus the camera)
-            Time.timeScale = 0f;
-
-            // 2) Clear UI icons
             itemsUpdater.ClearAll();
-
-            // 3) Activate the existing doll object and start its animation
-            dollAnimationScript.gameObject.SetActive(true);
-            dollAnimationScript.PlayAnimation(); 
-
-            // 4) Wait 2 real-time seconds (ignores timeScale)
-            yield return new WaitForSecondsRealtime(2f);
-
-            // 5) Unfreeze time
-            Time.timeScale = 1f;
-
-            // 6) Invoke the callback for getting to top down state
             callback?.Invoke();
+            
         }
+        
     }
 }
