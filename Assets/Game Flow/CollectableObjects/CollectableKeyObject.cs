bce3d4f0c72232621a25d784bf6d6d947686b841
tamirMoshkovitz <tamir.moshkovitz@mail.huji.ac.il>
2025-05-04 22:15:47 +0300

    Added more audio

--- Assets/Game Flow/CollectableObjects/CollectableKeyObject.cs
+++ Assets/Game Flow/CollectableObjects/CollectableKeyObject.cs
@@ -1,4 +1,8 @@
-﻿using Game_Flow.ImpactObjects.Scripts.Types;
+﻿using System;
+using System.Collections;
+using Core.Audio;
+using Game_Flow.ImpactObjects.Scripts.Audio;
+using Game_Flow.ImpactObjects.Scripts.Types;
 using Game_Flow.UI;
 using UnityEngine;
 
@@ -9,11 +13,30 @@ namespace Game_Flow.CollectableObjects
         [SerializeField] private OpenCloseImpactObject showcaseDoor1;
         [SerializeField] private OpenCloseImpactObject showcaseDoor2;
         [SerializeField] private ItemsUpdater itemsUpdater;
+        
+        [Header("Audio")]
+        [SerializeField] private AudioSource audioSource;
+        [SerializeField] private AudioClip audioClip;
+        
+        private OpenCloseObjectAudio _objectAudio;
+
+        private void Start()
+        {
+            _objectAudio = new OpenCloseObjectAudio(audioSource, audioClip);
+        }
+
         public void OnCollect()
         {
             showcaseDoor1.IsLocked = false;
             showcaseDoor2.IsLocked = false;
             itemsUpdater.AddItem(2);
+            _objectAudio.PlaySound();
+            StartCoroutine(DelayDestroy());
+        }
+        
+        private IEnumerator DelayDestroy()
+        {
+            yield return new WaitForSeconds(0.5f);
             Destroy(gameObject);
         }
     }
--- Assets/Game Flow/CollectableObjects/SwitchObject.cs
+++ Assets/Game Flow/CollectableObjects/SwitchObject.cs
@@ -1,3 +1,6 @@
+using System.Collections;
+using Core.Audio;
+using Game_Flow.ImpactObjects.Scripts.Audio;
 using UnityEngine;
 
 namespace Game_Flow.CollectableObjects
@@ -5,9 +8,27 @@ namespace Game_Flow.CollectableObjects
     public class SwitchObject:MonoBehaviour
     {
         [SerializeField] private GameObject objectToSwitch;
+        [SerializeField] private AudioSource audioSource;
+        [SerializeField] private AudioClip clip;
+        
+        private OpenCloseObjectAudio _objectAudio;
+        
+        private void Start()
+        {
+            _objectAudio = new OpenCloseObjectAudio(audioSource, clip);
+            _objectAudio.SetVolume(0.3f);
+        }
         public void ControlLights()
         {
             objectToSwitch.SetActive(!objectToSwitch.activeSelf);
+            StartCoroutine(PlaySound());
+        }
+
+        private IEnumerator PlaySound()
+        {
+            _objectAudio.PlaySound();
+            yield return new WaitForSeconds(.5f);
+            _objectAudio.StopSound();
         }
     }
 }
