b3eb5719d1132b3fa97a3244f3ba0c1da3bcd64a
yuvalarie <yuvalarie@gmail.com>
2025-05-10 13:39:22 +0300

    Added final scene assets + added to build profile

---
+++ Assets/Core/Managers/GameRestart.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Core.Managers
+{
+    public class GameRestart : MonoBehaviour
+    {
+        public void RestartGame()
+        {
+            Debug.Log("Restarting Game");
+            SceneManager.LoadScene("Sketch room");
+        }
+    }
+}
