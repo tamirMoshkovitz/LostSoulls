44c8e2236feff9ede84041ef958d3242a8a5a370
tamirMoshkovitz <tamir.moshkovitz@mail.huji.ac.il>
2025-05-12 05:01:39 +0300

    Doll collected audio

--- Assets/Game Flow/CollectableObjects/CollectableDollObject.cs
+++ Assets/Game Flow/CollectableObjects/CollectableDollObject.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections;
+using Game_Flow.ImpactObjects.Scripts.Audio;
 using Game_Flow.ImpactObjects.Scripts.Types;
 using Game_Flow.UI;
 using Unity.VisualScripting;
@@ -10,6 +13,22 @@ namespace Game_Flow.CollectableObjects
         [SerializeField] private OpenCloseImpactObject showcaseDoor1;
         [SerializeField] private OpenCloseImpactObject showcaseDoor2;
         [SerializeField] private ItemsUpdater itemsUpdater;
+        
+        [Header("Audio")]
+        [SerializeField] private AudioSource audioSource;
+        [SerializeField] private AudioClip audioClip;
+        
+        private OpenCloseObjectAudio _objectAudio;
+
+        private void Start()
+        {
+            if (audioSource != null && audioClip != null)
+            {
+                _objectAudio = new OpenCloseObjectAudio(audioSource, audioClip);
+                _objectAudio.SetVolume(0.5f);
+            }
+        }
+
         public void OnCollect()
         {
             if (showcaseDoor1.IsLocked || showcaseDoor2.IsLocked)
@@ -17,7 +36,22 @@ namespace Game_Flow.CollectableObjects
                 return;
             }
             itemsUpdater.AddItem(1);
-            Destroy(gameObject);
+            _objectAudio?.PlaySound();
+            StartCoroutine(DelayDestroy());
         }
-    }
+        
+        private IEnumerator DelayDestroy()
+        {
+            GetComponent<Collider>().enabled = false;
+            foreach (var renderer in GetComponentsInChildren<MeshRenderer>())
+            {
+                renderer.enabled = false;
+            }
+            foreach (Transform child in GetComponentInChildren<Transform>())
+            {
+                child.gameObject.SetActive(false);
+            }
+            yield return new WaitForSeconds(0.5f);
+            Destroy(gameObject);
+        }    }
 }
